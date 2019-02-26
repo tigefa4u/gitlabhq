1fd13a7fe3bab21df9cccfebfbd9e6389a67fdda
Robert Speicher <rspeicher@gmail.com>
2019-02-26 15:31:22 -0800

    Make lint-conflicts platform-agnostic
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/51124

--- scripts/lint-conflicts.sh
+++ scripts/lint-conflicts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-output=`git ls-files -z | grep -zvE '\.(rb|js|haml)$' | xargs -0n1 grep -HEn '^<<<<<<< '`
-echo $output
+output=$(git grep -E '^<<<<<<< HEAD' -- '*.haml' '*.js' '*.rb')
+echo "$output"
 test -z "$output"
