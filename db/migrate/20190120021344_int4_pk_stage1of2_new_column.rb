e3cc4dddf0d94175ca6d223e663daa43357d1c48
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2019-03-04 23:09:09 +0000

    Distinguish trigger functions

--- db/migrate/20190120021344_int4_pk_stage1of2_new_column.rb
+++ db/migrate/20190120021344_int4_pk_stage1of2_new_column.rb
@@ -11,17 +11,17 @@ class Int4PkStage1of2NewColumn < ActiveRecord::Migration[5.0]
     add_column(:ci_build_trace_sections, :id_new, :bigint)
 
     if Gitlab::Database.postgresql?
-      install_rename_triggers_for_postgresql(:int4_to_int8, :events, :id, :id_new, 'INSERT')
-      install_rename_triggers_for_postgresql(:int4_to_int8, :push_event_payloads, :event_id, :event_id_new, 'INSERT')
-      install_rename_triggers_for_postgresql(:int4_to_int8, :ci_build_trace_sections, :id, :id_new, 'INSERT')
+      install_rename_triggers_for_postgresql(:t_events_int4_to_int8, :events, :id, :id_new, 'INSERT')
+      install_rename_triggers_for_postgresql(:t_push_event_payloads_int4_to_int8, :push_event_payloads, :event_id, :event_id_new, 'INSERT')
+      install_rename_triggers_for_postgresql(:t_ci_build_trace_sections_int4_to_int8, :ci_build_trace_sections, :id, :id_new, 'INSERT')
     end
   end
 
   def down
     if Gitlab::Database.postgresql?
-      remove_rename_triggers_for_postgresql(:ci_build_trace_sections, :'int4_to_int8')
-      remove_rename_triggers_for_postgresql(:push_event_payloads, :'int4_to_int8')
-      remove_rename_triggers_for_postgresql(:events, :'int4_to_int8')
+      remove_rename_triggers_for_postgresql(:t_ci_build_trace_sections_int4_to_int8, :'int4_to_int8')
+      remove_rename_triggers_for_postgresql(:t_push_event_payloads_int4_to_int8, :'int4_to_int8')
+      remove_rename_triggers_for_postgresql(:t_events_int4_to_int8, :'int4_to_int8')
     end
 
     remove_column(:ci_build_trace_sections, :id_new)
