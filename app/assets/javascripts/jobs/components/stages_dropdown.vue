021f787eac9eea992e159e2e46de73f373f8e5c7
Shinya Maeda <shinya@gitlab.com>
2019-03-07 17:04:00 +0700

    Extend job detail view sidebar for mr pipelines

--- app/assets/javascripts/jobs/components/stages_dropdown.vue
+++ app/assets/javascripts/jobs/components/stages_dropdown.vue
@@ -42,11 +42,21 @@ export default {
     <a :href="pipeline.path" class="js-pipeline-path link-commit qa-pipeline-path"
       >#{{ pipeline.id }}</a
     >
-    <template v-if="hasRef">
-      {{ __('from') }}
-      <a :href="pipeline.ref.path" class="link-commit ref-name">{{ pipeline.ref.name }}</a>
-    </template>
-
+    if pipeline.merge_request.exist? {
+      for
+      <a :href="pipeline.merge_request.path" class="blah" >{{ pipeline.merge_request.iid }}</a>
+      with
+      <a :href="pipeline.merge_request.source_branch_path" class="blah">{{ pipeline.merge_request.source_branch }}</a>
+      if pipeline.merge_request["flags"]["attached"] {
+          into
+          <a :href="pipeline.merge_request.target_branch_path" class="blah">{{ pipeline.merge_request.target_branch }}</a>
+      }
+    } else {
+      <template v-if="hasRef">
+        {{ __('for') }}
+        <a :href="pipeline.ref.path" class="link-commit ref-name">{{ pipeline.ref.name }}</a>
+      </template>
+    }
     <button
       type="button"
       data-toggle="dropdown"
