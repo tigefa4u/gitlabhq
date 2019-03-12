6d54c544d5e89b86d6f9988112bd2374e01c1f4b
Roman Verevkin <verevkin.roma@gmail.com>
2019-03-12 21:53:07 +0300

    Fix test with empty snippets state

--- qa/qa/page/dashboard/snippet/index.rb
+++ qa/qa/page/dashboard/snippet/index.rb
@@ -3,12 +3,15 @@ module QA
     module Dashboard
       module Snippet
         class Index < Page::Base
-          view 'app/views/dashboard/_snippets_head.html.haml' do
-            element :new_snippet_button
+
+          view 'app/views/layouts/header/_new_dropdown.haml' do
+            element :new_menu_toggle
+            element :global_new_snippet_link
           end
 
           def go_to_new_snippet_page
-            click_element :new_snippet_button
+            click_element :new_menu_toggle
+            click_element :global_new_snippet_link
           end
         end
       end
