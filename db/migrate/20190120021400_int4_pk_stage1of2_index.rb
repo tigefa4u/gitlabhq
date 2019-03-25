fe49e5c5df43c33892878cc9c66055f27793e823
Felipe Artur <felipefac@gmail.com>
2019-03-25 10:51:07 -0300

    Improve indexes migration code

--- db/migrate/20190120021400_int4_pk_stage1of2_index.rb
+++ db/migrate/20190120021400_int4_pk_stage1of2_index.rb
@@ -11,37 +11,32 @@ class Int4PkStage1of2Index < ActiveRecord::Migration[5.0]
     # Create 3 helper indexes to speed up background migrations.
     # These indexes are to be removed in the next release.
 
-    remove_concurrent_index_by_name(:events, :events_int4_to_int8_helper)
     # Time estimate for GitLab.com: ~420s (~7 min)
-    add_concurrent_index(
-      :events,
-      :id,
-      name: :events_int4_to_int8_helper,
-      where: 'id_new is null'
-    )
+    add_index_for_new_field(:events, :id)
 
-    remove_concurrent_index_by_name(:events, :push_event_payloads_int4_to_int8_helper)
     # Time estimate for GitLab.com: ~360s (~6 min)
-    add_concurrent_index(
-      :push_event_payloads,
-      :event_id,
-      name: :push_event_payloads_int4_to_int8_helper,
-      where: 'event_id_new is null'
-    )
+    add_index_for_new_field(:push_event_payloads, :event_id)
 
-    remove_concurrent_index_by_name(:ci_build_trace_sections, :ci_build_trace_sections_int4_to_int8_helper)
     # Time estimate for GitLab.com: ~840s (~14 min)
-    add_concurrent_index(
-      :ci_build_trace_sections,
-      :id,
-      name: :ci_build_trace_sections_int4_to_int8_helper,
-      where: 'id_new is null'
-    )
+    add_index_for_new_field(:ci_build_trace_sections, :id)
   end
 
   def down
     remove_concurrent_index_by_name(:ci_build_trace_sections, :ci_build_trace_sections_int4_to_int8_helper)
-    remove_concurrent_index_by_name(:events, :push_event_payloads_int4_to_int8_helper)
+    remove_concurrent_index_by_name(:push_event_payloads, :push_event_payloads_int4_to_int8_helper)
     remove_concurrent_index_by_name(:events, :events_int4_to_int8_helper)
+  end
+
+  def add_index_for_new_field(table, field)
+    name = "#{table}_int4_to_int8_helper"
+
+    remove_concurrent_index_by_name(table, name)
+
+    add_concurrent_index(
+      table,
+      field,
+      name: name,
+      where: "#{field}_new is null"
+    )
   end
 end
