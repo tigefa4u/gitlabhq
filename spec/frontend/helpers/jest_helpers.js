51d5d923bba767a231754495e7e5f63e8ae12eea
Martin Hanzel <mhanzel@gitlab.com>
2019-05-24 12:56:00 +0200

    Migrate old notes app test from Karma to Jest

---
+++ spec/frontend/helpers/jest_helpers.js
@@ -0,0 +1,19 @@
+/*
+jest_helpers.js
+
+This method provides convenience functions to help migrating from Karma/Jasmine
+to Jest.
+ */
+
+export function createSpyObj(baseName, methods) {
+  return methods.reduce((obj, method) => (
+    Object.assign(
+      obj,
+      { [method]: jest.fn().mockName(`${ baseName }#${ method }`) }
+      )
+  ), {});
+}
+
+export default {
+  createSpyObj,
+};
