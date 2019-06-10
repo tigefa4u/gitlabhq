e8f23e84af01b0c0e797aeaa364ac3e985648b04
Constance Okoghenun <constanceokoghenun@gmail.com>
2019-06-10 22:53:21 +0200

    Squash commits from 57402-issues-list-vue-component
    Added locale changes to issues list component
    
    Added constants and missing i18n
    
    Moved items to computed properties
    
    Added MAX_ASSIGNEES_RENDER constant
    
    Apply suggestion to app/assets/javascripts/issues/components/issue.vue
    
    Apply suggestion to app/assets/javascripts/issues/index.js
    
    Apply suggestion to app/assets/javascripts/issues/services/issues_service.js
    
    Apply suggestion to app/assets/javascripts/issues/stores/modules/issues_list/actions.js
    
    Updated image_path to take from haml
    
    Fixed tests to account for empty state props
    
    Updated gitlab.pot

---
+++ app/assets/javascripts/issues/constants.js
@@ -0,0 +1,9 @@
+export const ISSUE_STATES = {
+  OPENED: 'opened',
+  CLOSED: 'closed',
+};
+
+export const ACTIVE_TAB_CLASS = 'active';
+export const ISSUES_PER_PAGE = 20;
+export const DASHBOARD_PAGE_NAME = 'dashboard';
+export const MAX_ASSIGNEES_RENDER = 3;
