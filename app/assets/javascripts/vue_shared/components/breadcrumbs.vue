581edabd749ead70233480b90697f57c4a614079
lauraMon <lmontemayor@gitlab.com>
2019-06-11 15:05:32 -0400

    Adds wrapper for gl-breadcrumb component
    This MR adds a wrapper to be used with the existing
    breadcrumb component from gitlab-ui.

---
+++ app/assets/javascripts/vue_shared/components/breadcrumbs.vue
@@ -0,0 +1,32 @@
+<script>
+import { GlBreadcrumb } from '@gitlab/ui';
+import Icon from '../../vue_shared/components/icon.vue';
+
+export default {
+  name: 'Breadcrumbs',
+  components: {
+    Icon,
+    GlBreadcrumb,
+  },
+  inheritsAttrs: false,
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [{ text: '', href: '' }],
+    },
+    imageSrc: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-breadcrumb :items="items" class="breadcrumbs">
+    <img slot="avatar" alt="avatar" class="breadcrumbs-avatar-tile" :src="imageSrc" />
+    <icon slot="caret" name="angle-right" :size="8" />
+  </gl-breadcrumb>
+</template>
