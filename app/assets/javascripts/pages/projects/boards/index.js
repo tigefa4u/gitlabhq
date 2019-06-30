73a2314d470c4a5597e53a501ef9a94923ded86b
Patrick Derichs <pderichs@gitlab.com>
2019-06-30 14:17:42 +0200

    Moving multiple issues (test ui)

--- app/assets/javascripts/pages/projects/boards/index.js
+++ app/assets/javascripts/pages/projects/boards/index.js
@@ -1,9 +1,40 @@
 import UsersSelect from '~/users_select';
 import ShortcutsNavigation from '~/behaviors/shortcuts/shortcuts_navigation';
 import initBoards from '~/boards';
+import $ from 'jquery';
+import axios from '~/lib/utils/axios_utils';
 
 document.addEventListener('DOMContentLoaded', () => {
   new UsersSelect(); // eslint-disable-line no-new
   new ShortcutsNavigation(); // eslint-disable-line no-new
   initBoards();
+
+  // TODO: Just for testing - remove this. (pd)
+    $('.btn-patricks-test-367346').click(function() {
+        var response;
+        var data = {
+            ids: [183, 184, 185],
+            from_list_id: 5,
+            to_list_id: 7,
+            move_before_id: 186,
+            move_after_id: null
+        };
+        axios.put("/-/boards/2/issues/move_multiple", data).then(({ response }) => {
+
+        });
+    });
+
+    $('.btn-patricks-test-367347').click(function() {
+        var response;
+        var data = {
+            id: 183,
+            from_list_id: 5,
+            to_list_id: 7,
+            move_before_id: 186
+        };
+        axios.put("/-/boards/2/issues/183", data).then(({ response }) => {
+
+        });
+    });
+
 });
