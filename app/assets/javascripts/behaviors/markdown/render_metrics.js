2140780e16bed29bc45d9501efa951d23e0a7420
Tristan Read <tread@gitlab.com>
2019-07-16 22:31:16 +0200

    Reword conditional logic

--- app/assets/javascripts/behaviors/markdown/render_metrics.js
+++ app/assets/javascripts/behaviors/markdown/render_metrics.js
@@ -4,7 +4,7 @@ import { createStore } from '~/monitoring/stores';
 
 // TODO: Handle copy-pasting - https://gitlab.com/gitlab-org/gitlab-ce/issues/64369.
 export default function renderMetrics(elements) {
-  if (elements.length < 1) {
+  if (!elements.length) {
     return;
   }
 
