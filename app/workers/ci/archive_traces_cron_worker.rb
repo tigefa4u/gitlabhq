b6db9a4d78ae873d0e94f0070ca26ae5e6742ebe
Shinya Maeda <shinya@gitlab.com>
2019-07-19 18:02:32 +0700

    Use ArchiveTraceWorker in ArchiveTracesCronWorker
    Currently, ArchiveTracesCronWorker keeps respawing repeatedly
    in a short interval because it's killed by Sidekiq Memory Killer.
    We split the memory consumption into multiple sidekiq jobs
    in order to avoid the unstable performance.

--- app/workers/ci/archive_traces_cron_worker.rb
+++ app/workers/ci/archive_traces_cron_worker.rb
@@ -11,7 +11,18 @@ module Ci
       # This could happen when ArchiveTraceWorker sidekiq jobs were lost by receiving SIGKILL
       # More details in https://gitlab.com/gitlab-org/gitlab-ce/issues/36791
       Ci::Build.finished.with_live_trace.find_each(batch_size: 100) do |build|
-        Ci::ArchiveTraceService.new.execute(build)
+        if Feature.enabled?(:ci_archive_trace_async)
+          ArchiveTraceWorker.perform_async(build.id)
+        else
+          begin
+            Ci::ArchiveTraceService.new.execute(build)
+          ensure
+            ##
+            # This is the temporary solution for avoiding the memory bloat.
+            # See more https://gitlab.com/gitlab-com/gl-infra/infrastructure/issues/4667#note_192401907
+            GC.start if Feature.enabled?(:ci_archive_trace_force_gc, default_enabled: true)
+          end
+        end
       end
     end
     # rubocop: enable CodeReuse/ActiveRecord
