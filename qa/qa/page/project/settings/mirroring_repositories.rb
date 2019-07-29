ryanfreckleton/gitlab-ce

Add e2e test: SSH push mirroring with private key
Adds an end-to-end test of push mirroring a repository over SSH using
a private key

qa/qa/page/project/settings/mirroring_repositories.rb
@@ -15,7 +15,9 @@ module QA
             element :mirror_repository_button
             element :mirror_repository_url
             element :mirror_last_update_at
+            element :mirror_error_badge
             element :mirrored_repository_row
+            element :copy_public_key_button
           end
 
           view 'app/views/projects/mirrors/_mirror_repos_form.html.haml' do
@@ -24,6 +26,16 @@ module QA
 
           view 'app/views/shared/_remote_mirror_update_button.html.haml' do
             element :update_now_button
+          end
+
+          view 'app/views/projects/mirrors/_ssh_host_keys.html.haml' do
+            element :detect_host_keys
+            element :fingerprints
+          end
+
+          view 'app/views/projects/mirrors/_authentication_method.html.haml' do
+            element :authentication_method
+            element :password
           end
 
           def repository_url=(value)
@@ -35,15 +47,33 @@ module QA
           end
 
           def mirror_direction=(value)
-            raise ArgumentError, "Mirror direction must be :push or :pull" unless [:push, :pull].include? value
+            raise ArgumentError, "Mirror direction must be 'Push' or 'Pull'" unless %w(Push Pull).include? value
 
             select_element(:mirror_direction, value)
           end
 
           def authentication_method=(value)
-            raise ArgumentError, "Authentication method must be :password or :none" unless [:password, :none].include? value
+            raise ArgumentError, "Authentication method must be 'SSH public key', 'Password', or 'None'" unless %w(Password None SSH\ public\ key).include? value
 
             select_element(:authentication_method, value)
+          end
+
+          def public_key(url)
+            row_index = find_repository_row_index url
+
+            within_element_by_index(:mirrored_repository_row, row_index) do
+              find_element(:copy_public_key_button)['data-clipboard-text']
+            end
+          end
+
+          def detect_host_keys
+            click_element :detect_host_keys
+
+            # The host key detection process is interrupted if we navigate away
+            # from the page before the fingerprint appears.
+            wait(max: 5) do
+              find_element(:fingerprints).has_text? /.*/
+            end
           end
 
           def mirror_repository
@@ -72,16 +102,19 @@ module QA
             # Fail early if the page still shows that there has been no update
             within_element_by_index(:mirrored_repository_row, row_index) do
               find_element(:mirror_last_update_at, wait: 0).assert_no_text('Never')
+              assert_no_element(:mirror_error_badge)
             end
           end
 
           private
 
           def find_repository_row_index(target_url)
-            all_elements(:mirror_repository_url).index do |url|
-              # The url might be a sanitized url but the target_url won't be so
-              # we compare just the paths instead of the full url
-              URI.parse(url.text).path == target_url.path
+            wait(max: 5, reload: false) do
+              all_elements(:mirror_repository_url).index do |url|
+                # The url might be a sanitized url but the target_url won't be so
+                # we compare just the paths instead of the full url
+                URI.parse(url.text).path == target_url.path
+              end
             end
           end
         end

qa/qa/specs/features/browser_ui/3_create/repository/push_mirroring_over_ssh_with_key_spec.rb
@@ -2,7 +2,7 @@
 
 module QA
   context 'Create' do
-    describe 'Push mirror a repository over HTTP' do
+    describe 'Push mirror a repository over SSH with a private key' do
       it 'configures and syncs a (push) mirrored repository'  do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
         Page::Main::Login.perform(&:sign_in_using_credentials)
@@ -10,8 +10,7 @@ module QA
         target_project = Resource::Project.fabricate! do |project|
           project.name = 'push-mirror-target-project'
         end
-        target_project_uri = target_project.repository_http_location.uri
-        target_project_uri.user = Runtime::User.username
+        target_project_uri = target_project.repository_ssh_location.uri
 
         source_project_push = Resource::Repository::ProjectPush.fabricate! do |push|
           push.file_name = 'README.md'
@@ -19,18 +18,35 @@ module QA
           push.commit_message = 'Add README.md'
         end
         source_project_push.project.visit!
-
         Page::Project::Show.perform(&:wait_for_push)
 
+        # Configure the source project to push to the target project and get
+        # the public key to be used as a deploy key
+        Page::Project::Menu.perform(&:click_repository_settings)
+        public_key = Page::Project::Settings::Repository.perform do |settings|
+          settings.expand_mirroring_repositories do |mirror_settings|
+            mirror_settings.repository_url = target_project_uri
+            mirror_settings.mirror_direction = 'Push'
+            mirror_settings.authentication_method = 'SSH public key'
+            mirror_settings.detect_host_keys
+            mirror_settings.mirror_repository
+            mirror_settings.public_key target_project_uri
+          end
+        end
+
+        # Add the public key to the target project as a deploy key
+        Resource::DeployKey.fabricate! do |resource|
+          resource.project = target_project
+          resource.title = "push mirror key #{Time.now.to_f}"
+          resource.key = public_key
+          resource.allow_write_access = true
+        end
+
+        # Sync the repositories
+        source_project_push.project.visit!
         Page::Project::Menu.perform(&:click_repository_settings)
         Page::Project::Settings::Repository.perform do |settings|
           settings.expand_mirroring_repositories do |mirror_settings|
-            # Configure the source project to push to the target project
-            mirror_settings.repository_url = target_project_uri
-            mirror_settings.mirror_direction = 'Push'
-            mirror_settings.authentication_method = 'Password'
-            mirror_settings.password = Runtime::User.password
-            mirror_settings.mirror_repository
             mirror_settings.update target_project_uri
           end
         end

tigefa4u/gitlabhq

qa/qa/page/project/settings/mirroring_repositories.rb
@@ -15,7 +15,9 @@ module QA
             element :mirror_repository_button
             element :mirror_repository_url
             element :mirror_last_update_at
+            element :mirror_error_badge
             element :mirrored_repository_row
+            element :copy_public_key_button
           end
 
           view 'app/views/projects/mirrors/_mirror_repos_form.html.haml' do
@@ -24,6 +26,16 @@ module QA
 
           view 'app/views/shared/_remote_mirror_update_button.html.haml' do
             element :update_now_button
+          end
+
+          view 'app/views/projects/mirrors/_ssh_host_keys.html.haml' do
+            element :detect_host_keys
+            element :fingerprints_list
+          end
+
+          view 'app/views/projects/mirrors/_authentication_method.html.haml' do
+            element :authentication_method
+            element :password
           end
 
           def repository_url=(value)
@@ -35,15 +47,33 @@ module QA
           end
 
           def mirror_direction=(value)
-            raise ArgumentError, "Mirror direction must be :push or :pull" unless [:push, :pull].include? value
+            raise ArgumentError, "Mirror direction must be 'Push' or 'Pull'" unless %w(Push Pull).include? value
 
             select_element(:mirror_direction, value)
           end
 
           def authentication_method=(value)
-            raise ArgumentError, "Authentication method must be :password or :none" unless [:password, :none].include? value
+            raise ArgumentError, "Authentication method must be 'SSH public key', 'Password', or 'None'" unless %w(Password None SSH\ public\ key).include? value
 
             select_element(:authentication_method, value)
+          end
+
+          def public_key(url)
+            row_index = find_repository_row_index url
+
+            within_element_by_index(:mirrored_repository_row, row_index) do
+              find_element(:copy_public_key_button)['data-clipboard-text']
+            end
+          end
+
+          def detect_host_keys
+            click_element :detect_host_keys
+
+            # The host key detection process is interrupted if we navigate away
+            # from the page before the fingerprint appears.
+            wait(max: 5) do
+              find_element(:fingerprints_list).has_text? /.*/
+            end
           end
 
           def mirror_repository
@@ -72,16 +102,19 @@ module QA
             # Fail early if the page still shows that there has been no update
             within_element_by_index(:mirrored_repository_row, row_index) do
               find_element(:mirror_last_update_at, wait: 0).assert_no_text('Never')
+              assert_no_element(:mirror_error_badge)
             end
           end
 
           private
 
           def find_repository_row_index(target_url)
-            all_elements(:mirror_repository_url).index do |url|
-              # The url might be a sanitized url but the target_url won't be so
-              # we compare just the paths instead of the full url
-              URI.parse(url.text).path == target_url.path
+            wait(max: 5, reload: false) do
+              all_elements(:mirror_repository_url).index do |url|
+                # The url might be a sanitized url but the target_url won't be so
+                # we compare just the paths instead of the full url
+                URI.parse(url.text).path == target_url.path
+              end
             end
           end
         end

qa/qa/specs/features/browser_ui/3_create/repository/push_mirroring_over_ssh_with_key_spec.rb
@@ -2,7 +2,7 @@
 
 module QA
   context 'Create' do
-    describe 'Push mirror a repository over HTTP' do
+    describe 'Push mirror a repository over SSH with a private key' do
       it 'configures and syncs a (push) mirrored repository' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
         Page::Main::Login.perform(&:sign_in_using_credentials)
@@ -10,8 +10,7 @@ module QA
         target_project = Resource::Project.fabricate! do |project|
           project.name = 'push-mirror-target-project'
         end
-        target_project_uri = target_project.repository_http_location.uri
-        target_project_uri.user = Runtime::User.username
+        target_project_uri = target_project.repository_ssh_location.uri
 
         source_project_push = Resource::Repository::ProjectPush.fabricate! do |push|
           push.file_name = 'README.md'
@@ -20,15 +19,33 @@ module QA
         end
         source_project_push.project.visit!
 
+        # Configure the source project to push to the target project and get
+        # the public key to be used as a deploy key
+        Page::Project::Menu.perform(&:go_to_repository_settings)
+        public_key = Page::Project::Settings::Repository.perform do |settings|
+          settings.expand_mirroring_repositories do |mirror_settings|
+            mirror_settings.repository_url = target_project_uri
+            mirror_settings.mirror_direction = 'Push'
+            mirror_settings.authentication_method = 'SSH public key'
+            mirror_settings.detect_host_keys
+            mirror_settings.mirror_repository
+            mirror_settings.public_key target_project_uri
+          end
+        end
+
+        # Add the public key to the target project as a deploy key
+        Resource::DeployKey.fabricate! do |resource|
+          resource.project = target_project
+          resource.title = "push mirror key #{Time.now.to_f}"
+          resource.key = public_key
+          resource.allow_write_access = true
+        end
+
+        # Sync the repositories
+        source_project_push.project.visit!
         Page::Project::Menu.perform(&:go_to_repository_settings)
         Page::Project::Settings::Repository.perform do |settings|
           settings.expand_mirroring_repositories do |mirror_settings|
-            # Configure the source project to push to the target project
-            mirror_settings.repository_url = target_project_uri
-            mirror_settings.mirror_direction = 'Push'
-            mirror_settings.authentication_method = 'Password'
-            mirror_settings.password = Runtime::User.password
-            mirror_settings.mirror_repository
             mirror_settings.update target_project_uri
           end
         end