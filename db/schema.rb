327df52fda422c26653c70c911c7a02f8655490c
charlieablett <cablett@gitlab.com>
2019-08-07 13:04:27 +1200

    Remove default milestone reversion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_001141) do
+ActiveRecord::Schema.define(version: 2019_08_02_012622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
