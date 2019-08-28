1e7c73eddfc2294e1d2655c2ee5a928225252e46
Avielle Wolfe <awolfe@gitlab.com>
2019-08-28 17:29:57 -0400

    Finish writing specs
    * Build custom Artifact class for using security reports in specs
    * Extract create_artifact

--- spec/migrations/update_fingerprints_on_non_default_branch_feedback_spec.rb
+++ spec/migrations/update_fingerprints_on_non_default_branch_feedback_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require Rails.root.join('db', 'post_migrate', '20190827193201_update_fingerprints_on_non_default_branch_feedback.rb')
 
 describe UpdateFingerprintsOnNonDefaultBranchFeedback, :migration do
-  let(:artifacts) { table(:ci_job_artifacts) }
   let(:jobs) { table(:ci_builds) }
   let(:namespaces) { table(:namespaces) }
   let(:pipelines) { table(:ci_pipelines) }
@@ -10,39 +11,32 @@ describe UpdateFingerprintsOnNonDefaultBranchFeedback, :migration do
   let(:vulnerability_feedback) { table(:vulnerability_feedback) }
   let(:users) { table(:users) }
 
+  let(:job) { jobs.create }
   let(:namespace) { namespaces.create(name: 'gitlab', path: 'gitlab') }
   let(:pipeline) { pipelines.create }
   let(:project) { projects.create(name: 'gitlab', path: 'gitlab', namespace_id: namespace.id) }
   let(:user) { users.create(projects_limit: 1) }
 
-  let(:vulnerability) { ci_report['vulnerabilities'].first }
-  let(:ci_report) do
-    { 'vulnerabilities' => [
-      {
-        'cve' => 'new_fingerprint',
-        'featurename' => 'old_fingerprint',
-        'featureversion' => 'old_fingerprint',
-        'message' => 'old_fingerprint',
-        'namespace' => 'old_fingerprint',
-        'vulnerability' => 'new_fingerprint'
+  before(:all) do
+    artifacts = table(:ci_job_artifacts)
+
+    class Artifact < artifacts
+      mount_uploader :file, JobArtifactUploader
+
+      enum file_location: {
+        legacy_path: 1,
+        hashed_path: 2
       }
-    ] }
+    end
   end
 
   describe '#up' do
     it 'updates project_fingerprint on feedback for dependency scanning vulnerabilities' do
       report = fixture_file_upload(Rails.root.join(
-        'spec/fixtures/security_reports/master/gl-container-scanning-report.json'
+        'spec/fixtures/security_reports/master/gl-dependency-scanning-report.json'
       ), 'text/plain')
-      job = jobs.create
-      artifact = artifacts.create(
-        file: report,
-        file_format: 1,
-        file_type: 7,
-        job_id: job.id,
-        project_id: project.id
-      )
-      vulnerability = JSON.parse(artifact.file)['vulnerabilities'].first
+      artifact = create_artifact(file_type: 7, report: report)
+      vulnerability = JSON.parse(artifact.file.read)['vulnerabilities'].first
       old_fingerprint = Digest::SHA1.hexdigest(vulnerability['message'])
       new_fingerprint = Digest::SHA1.hexdigest(vulnerability['cve'])
       feedback = create_feedback(fingerprint: old_fingerprint)
@@ -54,17 +48,10 @@ describe UpdateFingerprintsOnNonDefaultBranchFeedback, :migration do
 
     it 'updates project_fingerprint on feedback for container scanning vulnerabilities' do
       report = fixture_file_upload(Rails.root.join(
-        'spec/fixtures/security_reports/master/gl-dependency-scanning-report.json'
+        'spec/fixtures/security_reports/master/gl-container-scanning-report.json'
       ), 'text/plain')
-      job = jobs.create
-      artifact = artifacts.create(
-        file: report,
-        file_format: 1,
-        file_type: 6,
-        job_id: job.id,
-        project_id: project.id
-      )
-      vulnerability = JSON.parse(artifact.file.body)['vulnerabilities'].first
+      artifact = create_artifact(file_type: 6, report: report)
+      vulnerability = JSON.parse(artifact.file.read)['vulnerabilities'].first
       old_fingerprint = Digest::SHA1.hexdigest(
         "#{vulnerability['namespace']}:#{vulnerability['vulnerability']}" \
         ":#{vulnerability['featurename']}:#{vulnerability['featureversion']}"
@@ -80,17 +67,10 @@ describe UpdateFingerprintsOnNonDefaultBranchFeedback, :migration do
     context 'when a feedback of the same type with a new fingerprint exists' do
       it 'deletes the old feedback' do
         report = fixture_file_upload(Rails.root.join(
-          'spec/fixtures/security_reports/master/gl-container-scanning-report.json'
+          'spec/fixtures/security_reports/master/gl-dependency-scanning-report.json'
         ), 'text/plain')
-        job = jobs.create
-        artifact = artifacts.create(
-          file: report,
-          file_format: 1,
-          file_type: 7,
-          job_id: job.id,
-          project_id: project.id
-        )
-        vulnerability = JSON.parse(artifact.file.body)['vulnerabilities'].first
+        artifact = create_artifact(file_type: 7, report: report)
+        vulnerability = JSON.parse(artifact.file.read)['vulnerabilities'].first
         old_fingerprint = Digest::SHA1.hexdigest(vulnerability['message'])
         new_fingerprint = Digest::SHA1.hexdigest(vulnerability['cve'])
         old_feedback = create_feedback(fingerprint: old_fingerprint)
@@ -101,6 +81,17 @@ describe UpdateFingerprintsOnNonDefaultBranchFeedback, :migration do
         expect { old_feedback.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
+  end
+
+  def create_artifact(file_type:, report:)
+    Artifact.create(
+      file: report,
+      file_format: 1,
+      file_location: 2,
+      file_type: file_type,
+      job_id: job.id,
+      project_id: project.id
+    )
   end
 
   def create_feedback(fingerprint:)
