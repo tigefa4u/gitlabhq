22722b9f9fba1b20405e6a286324ac878e4061c3
John Cai <jcai@gitlab.com>
2019-09-03 13:39:13 -0700

    Deprecate add namespace calll

--- app/models/storage/hashed_project.rb
+++ app/models/storage/hashed_project.rb
@@ -28,7 +28,7 @@ module Storage
     end
 
     def ensure_storage_path_exists
-      gitlab_shell.add_namespace(repository_storage, base_dir)
+      #gitlab_shell.add_namespace(repository_storage, base_dir)
     end
 
     def rename_repo(old_full_path: nil, new_full_path: nil)
