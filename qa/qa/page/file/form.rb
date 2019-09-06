0a2906f9cb503519827bcdee1677bb8728e22432
pburdette <pburdette@gitlab.com>
2019-09-06 08:11:26 -0400

    Fix qa error for template type dropdown

--- qa/qa/page/file/form.rb
+++ qa/qa/page/file/form.rb
@@ -14,7 +14,6 @@ module QA
         end
 
         view 'app/views/projects/blob/_template_selectors.html.haml' do
-          element :template_type_dropdown
           element :gitignore_dropdown
           element :gitlab_ci_yml_dropdown
           element :dockerfile_dropdown
@@ -23,25 +22,6 @@ module QA
 
         def add_name(name)
           fill_in 'file_name', with: name
-        end
-
-        def select_template(template_type, template)
-          click_element :template_type_dropdown
-          click_link template_type
-
-          case template_type
-          when '.gitignore'
-            click_element :gitignore_dropdown
-          when '.gitlab-ci.yml'
-            click_element :gitlab_ci_yml_dropdown
-          when 'Dockerfile'
-            click_element :dockerfile_dropdown
-          when 'LICENSE'
-            click_element :license_dropdown
-          else
-            raise %Q(Unsupported template_type "#{template_type}". Please confirm that it is a valid option.)
-          end
-          filter_and_select template
         end
       end
     end
