db4fd012a8d3e2334de129953f16b5139264e13e
Avielle Wolfe <awolfe@gitlab.com>
2019-09-09 19:48:08 -0400

    Address MR comments
    * remove unnecessary comments
    * prefer `delete` to `destroy`

--- db/post_migrate/20190827193201_update_fingerprints_on_non_default_branch_feedback.rb
+++ db/post_migrate/20190827193201_update_fingerprints_on_non_default_branch_feedback.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-# See http://doc.gitlab.com/ce/development/migration_style_guide.html
-# for more information on how to write migrations for GitLab.
-
 class UpdateFingerprintsOnNonDefaultBranchFeedback < ActiveRecord::Migration[5.2]
   include Gitlab::Database::MigrationHelpers
 
-  # Set this constant to true if this migration requires downtime.
   DOWNTIME = false
 
   disable_ddl_transaction!
@@ -18,7 +14,7 @@ class UpdateFingerprintsOnNonDefaultBranchFeedback < ActiveRecord::Migration[5.2
           begin
             feedback.update_fingerprint!
           rescue ActiveRecord::RecordNotUnique
-            feedback.destroy
+            feedback.delete
           end
         end
       end
