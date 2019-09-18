nickbrandt/GitLab

Add columns pages/artifacts size limits
* projects
* namespaces

db/migrate/20190911115056_add_projects_max_pages_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddProjectsMaxPagesSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :projects, :max_pages_size, :bigint
+  end
+end

db/migrate/20190911115109_add_namespaces_max_pages_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddNamespacesMaxPagesSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :namespaces, :max_pages_size, :bigint
+  end
+end

db/migrate/20190911115207_add_projects_max_artifacts_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddProjectsMaxArtifactsSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :projects, :max_artifacts_size, :bigint
+  end
+end

db/migrate/20190911115222_add_namespaces_max_artifacts_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddNamespacesMaxArtifactsSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :namespaces, :max_artifacts_size, :bigint
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_000130) do
+ActiveRecord::Schema.define(version: 2019_09_11_115222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -2273,6 +2273,8 @@ ActiveRecord::Schema.define(version: 2019_09_10_000130) do
     t.integer "last_ci_minutes_usage_notification_level"
     t.integer "subgroup_creation_level", default: 1
     t.boolean "emails_disabled"
+    t.bigint "max_pages_size"
+    t.bigint "max_artifacts_size"
     t.index ["created_at"], name: "index_namespaces_on_created_at"
     t.index ["custom_project_templates_group_id", "type"], name: "index_namespaces_on_custom_project_templates_group_id_and_type", where: "(custom_project_templates_group_id IS NOT NULL)"
     t.index ["file_template_project_id"], name: "index_namespaces_on_file_template_project_id"
@@ -2846,6 +2848,8 @@ ActiveRecord::Schema.define(version: 2019_09_10_000130) do
     t.boolean "merge_requests_disable_committers_approval"
     t.boolean "require_password_to_approve"
     t.boolean "emails_disabled"
+    t.bigint "max_pages_size"
+    t.bigint "max_artifacts_size"
     t.index ["archived", "pending_delete", "merge_requests_require_code_owner_approval"], name: "projects_requiring_code_owner_approval", where: "((pending_delete = false) AND (archived = false) AND (merge_requests_require_code_owner_approval = true))"
     t.index ["created_at"], name: "index_projects_on_created_at"
     t.index ["creator_id"], name: "index_projects_on_creator_id"

tigefa4u/gitlabhq

db/migrate/20190911115056_add_projects_max_pages_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddProjectsMaxPagesSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :projects, :max_pages_size, :integer
+  end
+end

db/migrate/20190911115109_add_namespaces_max_pages_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddNamespacesMaxPagesSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :namespaces, :max_pages_size, :integer
+  end
+end

db/migrate/20190911115207_add_projects_max_artifacts_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddProjectsMaxArtifactsSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :projects, :max_artifacts_size, :integer
+  end
+end

db/migrate/20190911115222_add_namespaces_max_artifacts_size.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddNamespacesMaxArtifactsSize < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    add_column :namespaces, :max_artifacts_size, :integer
+  end
+end

db/schema.rb
@@ -2274,6 +2274,8 @@ ActiveRecord::Schema.define(version: 2019_09_12_061145) do
     t.integer "last_ci_minutes_usage_notification_level"
     t.integer "subgroup_creation_level", default: 1
     t.boolean "emails_disabled"
+    t.integer "max_pages_size"
+    t.integer "max_artifacts_size"
     t.index ["created_at"], name: "index_namespaces_on_created_at"
     t.index ["custom_project_templates_group_id", "type"], name: "index_namespaces_on_custom_project_templates_group_id_and_type", where: "(custom_project_templates_group_id IS NOT NULL)"
     t.index ["file_template_project_id"], name: "index_namespaces_on_file_template_project_id"
@@ -2847,6 +2849,8 @@ ActiveRecord::Schema.define(version: 2019_09_12_061145) do
     t.boolean "service_desk_enabled", default: true
     t.integer "approvals_before_merge", default: 0, null: false
     t.boolean "emails_disabled"
+    t.integer "max_pages_size"
+    t.integer "max_artifacts_size"
     t.index ["archived", "pending_delete", "merge_requests_require_code_owner_approval"], name: "projects_requiring_code_owner_approval", where: "((pending_delete = false) AND (archived = false) AND (merge_requests_require_code_owner_approval = true))"
     t.index ["created_at"], name: "index_projects_on_created_at"
     t.index ["creator_id"], name: "index_projects_on_creator_id"